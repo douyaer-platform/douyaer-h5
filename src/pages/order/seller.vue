ab5a0e53363dd1565a7ee56c7813b244f25d0a57
weiberzeng <zengyuanman123@163.com>
2018-06-11 20:43:20 +0800

    build

--- src/pages/order/seller.vue
+++ src/pages/order/seller.vue
@@ -3,7 +3,7 @@
 * @Author: weiberzeng
 * @Date:   2018-04-25 14:35:32
 * @Last Modified by:   weiberzeng
-* @Last Modified time: 2018-04-25 14:38:09
+* @Last Modified time: 2018-06-11 20:39:16
 -->
 <template>
     <div class="page page-current">
@@ -12,6 +12,47 @@
         </header>
         <bottomBar></bottomBar>
         <div class="content">
+            <div class="main-box">
+                <div class="box-hd">
+                    <span class="text">待完成</span>
+                </div>
+                <div class="box-bd">
+                    <ul class="order-list">
+                        <li class="item clearfix">
+                            <div class="img-wrap">
+                                <img src="/static/demo/demo.jpg" alt="">
+                            </div>
+                            <div class="inner">
+                                <div class="name">商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称</div>
+                                <div class="state">
+                                    <span>加收藏后3-4天付款</span>
+                                    <span>假聊</span>
+                                    <span class="right">垫付</span>
+                                </div>
+                                <div class="money">
+                                    <span>￥</span>398
+                                </div>
+                            </div>
+                            <!-- <div class="detail">
+                                <span class="text">共计：</span>
+                                <span class="attr">
+                                    <span class="name">放单量</span>
+                                    <span class="val">3</span>
+                                </span>
+                                <span class="attr">
+                                    <span class="name">佣金</span>
+                                    <span class="val">3</span>
+                                </span>
+                            </div> -->
+                            <div class="ctrl">
+                                <a href="javascipt:;" class="button">立即投诉</a>
+                                <a href="javascipt:;" class="button">去完成</a>
+                                <a href="javascipt:;" class="button">取消订单</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
