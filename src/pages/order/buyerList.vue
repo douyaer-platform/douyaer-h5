0f6e824e0cc6e766547a6692528738c863eff305
weiberzeng <zengyuanman123@163.com>
2018-08-18 22:13:48 +0800

    bugfix

--- src/pages/order/buyerList.vue
+++ src/pages/order/buyerList.vue
@@ -3,13 +3,13 @@
 * @Author: weiberzeng
 * @Date:   2018-08-02 22:57:40
 * @Last Modified by:   weiberzeng
-* @Last Modified time: 2018-08-18 22:07:34
+* @Last Modified time: 2018-08-18 22:12:37
 -->
 <template>
     <div class="page page-current">
         <header class="bar bar-nav">
             <router-link to="/order/buyer" class="button button-link button-nav pull-left"><span class="icon icon-left"></span></router-link>
-            <h1 class="title">我的订单</h1>
+            <h1 class="title">我的任务</h1>
         </header>
         <bottomBar></bottomBar>
         <div class="sub-tab">
